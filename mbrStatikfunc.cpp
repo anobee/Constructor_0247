a5e5a69c465e1f4badb8b406ee9e81a8e8491ea6
anobee <HP@DESKTOP-KFAVCD8>
2024-05-14 15:17:49 +0700

    end2

--- mbrStatikfunc.cpp
+++ mbrStatikfunc.cpp
@@ -12,5 +12,45 @@ class mahasiswa
 
         void setID();
         void printAll();
-        
+
+        static void setNim (int pNim) {nim = pNim;}
+        static int getNIm() {return nim;}
+
+        mahasiswa (string pnama)
+        {
+            nama = pnama;
+            setID();
+        }
+};
+
+int mahasiswa::nim = 0;
+
+void mahasiswa::setID()
+{
+    id == ++nim;
+}
+
+void mahasiswa::printAll()
+{
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
+int main()
+{
+    mahasiswa mhs1("James");
+    mahasiswa mhs2("pengkor");
+    mahasiswa::setNim(247);
+    mahasiswa mhs3("Richard");
+    mahasiswa mhs4("baggio");
+
+    mhs1.printAll();
+    
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+
+    cout << "akses dari luar objek = " << mahasiswa::getNIm() << endl;
+    return 0;
 }
