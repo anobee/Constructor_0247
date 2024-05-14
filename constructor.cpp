ab1a53545f2dd3e5bc0a917af74824e091f259fd
anobee <HP@DESKTOP-KFAVCD8>
2024-05-14 15:42:14 +0700

    end2

--- constructor.cpp
+++ constructor.cpp
@@ -18,4 +18,41 @@ class mahasiswa
         {
             nim = iNim;
         }
+
+        mahasiswa(int iNiim)
+        {
+            nim = iNim;
+        };
+
+        mahasiswa(string iNama)
+        {
+            nama = iNama;
+        };
+
+        mahasiswa(int iNim, string iNama)
+        {
+            nim = iNim;
+            nama = iNama;
+        };
+
+        void cetak(){
+            cout << "NIM = " << nim << endl;
+            cout << "Nama = " << nama << endl;
+            cout << endl;
+        };
+};
+
+int main()
+{
+    mahasiswa mhs1;
+    mahasiswa mhs2(20);
+    mahasiswa mhs3("Richard");
+    mahasiswa mhs4(21,"brokir");
+
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
+
+    return 0;
 }
